1c3afc6846d4a21fccee6a08a56ab26288cdc9ba
bobinnard <skyland1360@gmail.com>
2025-12-23 14:15:54 +0100

    finished the map

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public int speed;
     [SerializeField] private CharacterController player;
+    [SerializeField] private Transform visor;
 
     // Update is called once per frame
     void Update()
     {
         player.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))*speed*Time.deltaTime);
+        if (Input.GetAxis("Horizontal") > 0) visor.LookAt(visor.position + gameObject.transform.right, visor.up);
+        if (Input.GetAxis("Horizontal") < 0) visor.LookAt(visor.position - gameObject.transform.right, visor.up);
+        if (Input.GetAxis("Vertical") > 0) visor.LookAt(visor.position + gameObject.transform.forward, visor.up);
+        if (Input.GetAxis("Vertical") < 0) visor.LookAt(visor.position - gameObject.transform.forward, visor.up);
     }
 }
