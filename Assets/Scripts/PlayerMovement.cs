2ef26878497350ab0599bd94087e75e2ebb5ed00
bobinnard <skyland1360@gmail.com>
2025-12-23 12:53:04 +0100

    Implemented working movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private CharacterController player;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0,0);
-        transform.position += new Vector3(0,0,Input.GetAxis("Vertical")*speed*Time.deltaTime);
+        player.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))*speed*Time.deltaTime);
     }
 }
